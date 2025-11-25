a58e2656da81852af3611204b253e2bf78c118ec
neutrino-dot <iz24919bd@isk.ed.jp>
2025-11-25 16:32:52 +0900

    Implement fade effect in voice synthesis
    Added apply_fade function to apply fade effects to the audio wave.

--- voice_synthesize.py
+++ voice_synthesize.py
@@ -1,7 +1,7 @@
 from music8bit import *
 from music8bit import WaveGenerator
 from music8bit.utils import _validate
-from pico_formants import formants
+from .pico_formants import formants
 import numpy as np
 from scipy.signal import square,butter,sosfilt
 
@@ -15,6 +15,17 @@ def bpf(wave,formant):
       filtered += sosfilt(sos, wave)
   return filtered
 
+def apply_fade(wave, fade_time):
+    """音の前後にフェードをかけて自然に音を変更するための関数"""
+    wave = np.atleast_1d(wave)
+    fade_samples = int(SAMPLING_RATE * fade_time)
+    fade_in = np.linspace(0, 1, fade_samples)
+    fade_out = np.linspace(1, 0, fade_samples)
+
+    if len(wave) > 2 * fade_samples:
+        wave[:fade_samples] *= fade_in
+        wave[-fade_samples:] *= fade_out
+    return wave
 
 class SyntheticVoice(WaveGenerator):
     """フォルマントにしたがって合成音声を作る部分"""
@@ -29,4 +40,4 @@ class SyntheticVoice(WaveGenerator):
         formant = formants[vowel[0]]
         mod = np.sin(2*np.pi*5*t) * int(self.vibrato) # ビブラート選択
         base_pulse =  square(2 * np.pi * freq * t,duty=0.25) # 基本振動(dutyは12.5%)
-        return np.array([bpf(base_pulse,formant)])
+        return np.array([apply_fade(bpf(base_pulse,formant),0.08)])
